81692851fec703d0e725293e65a4fa2050af5f0a
Rustin Player <rjplayer@outlook.com>
2019-04-19 01:10:23 -0400

    Changed Category datatype to enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190417035918) do
+ActiveRecord::Schema.define(version: 20190419035411) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20190417035918) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.string "category"
+    t.integer "category", default: 0
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
